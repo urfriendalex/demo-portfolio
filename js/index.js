76db1e0ff2a07cc551684418e3a513a09fb51cc8
aliaksandr.yansans <smizer_fun@mail.ru>
2019-10-03 21:34:04 +0200

    changed the idea removed js animations

--- js/index.js
+++ js/index.js
@@ -1,35 +1,36 @@
-var $circle = $('.circle'),
-    $links = $('.projects ul li a'),
-    $section= $('.projects');
+// var $circle = $('.circle'),
+//     $links = $('.projects ul li a'),
+//     $section= $('.projects');
 
-  function moveCircle(e) {
-    TweenLite.to($circle, 0.01, {
-      css: {
-        left: e.pageX,
-        top: e.pageY
-      }
-    });
-  }
+//   function moveCircle(e) {
+//     TweenLite.to($circle, 0.3, {
+//       css: {
+//         left: e.pageX,
+//         top: e.pageY
+//       }
+//     });
+//   }
 
-$($links).mouseover(function(){
-  TweenLite.to($circle,0.1,{scale:2.4})
-});
-$($section).on('mousemove', moveCircle);
-$($links).mouseout(function(){
-    TweenLite.to($circle,0.1,{scale:0.8})
-});
-$(".exit").mouseout(function(){
-    TweenLite.to($circle,0.1,{scale:0.8})
-});
-$('.exit').mouseover(function(){
-    TweenLite.to($circle,0.1,{scale:2})
-});
+// $($links).mouseover(function(){
+//   TweenLite.to($circle,0.1,{scale:2.4})
+// });
+// $($section).on('mousemove', moveCircle);
+// $($links).mouseout(function(){
+//     TweenLite.to($circle,0.1,{scale:0.8})
+// });
+// $(".exit").mouseout(function(){
+//     TweenLite.to($circle,0.1,{scale:0.8})
+// });
+// $('.exit').mouseover(function(){
+//     TweenLite.to($circle,0.1,{scale:2})
+// });
 
 
 $(".btn-wrapper a").click(function(){
-    $($section).toggleClass("visible");
+    $(".projects").toggleClass("visible");
 });
 
 $('.exit').click(function(){
-    $($section).toggleClass("visible");
+    $(".projects").toggleClass("visible");
 })
+
